094872263fd809fe6d95e3698b46bca8ce555281
Melonthedev <56476502+Melonthedev@users.noreply.github.com>
2024-06-13 13:47:42 +0200

    Fixed Massive Bug

--- security_system/security_system.ino
+++ security_system/security_system.ino
@@ -10,10 +10,10 @@
 #define RFID_RST_PIN A1
 #define SERVO A3
 #define KLINGEL_PIN A2
-#define BUZZER 3
-#define TASER_PIN 11
-#define GREEN_LIGHT 2
-#define RED_LIGHT 4
+#define BUZZER A1
+#define TASER_PIN A0
+#define GREEN_LIGHT 1
+#define RED_LIGHT 0
 
 // Keypad
 char C1, C2, C3, C4;
@@ -39,13 +39,13 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 struct AuthorizedUser {
   long chipId;
   String username;
-  char code[];
+  char code[4];
 };
 
 AuthorizedUser authorizedUsers[3] = {
-  {2048270, "Marlon", {'1', '2', '3', 'A'}},
-  {480830, "Ferdi", {'1', '2', '3', 'A'}},
-  {2562190, "Blau", {'1', '2', '3', 'A'}}
+  {2562190, "Marlon", {'1', '2', '3', 'A'}},
+  {389620, "Ferdi", {'A', 'B', 'B', 'A'}},
+  {2076320, "Jakob", {'3', '1', '4', '8'}}
 };
 
 int remainingAttempts = 3;
@@ -77,6 +77,24 @@ AuthorizedUser* getUserByCardId(long id) {
   return NULL;
 }
 
+void printCenteredText(String text, String text2) {
+  lcd.clear();
+  if (text != "") {
+    int cursorPos = (16 - text.length()) / 2; 
+    lcd.setCursor(cursorPos, 0);
+    lcd.print(text);
+  }
+  if (text2 != "") {
+    int cursorPos = (16 - text2.length()) / 2; 
+    lcd.setCursor(cursorPos, 1);
+    lcd.print(text2);
+  }
+}
+
+void printCenteredText(String text) {
+  printCenteredText(text, "");
+}
+
 void accessGrantedSound() {
   tone(BUZZER, NOTE_E7);
   delay(50);
@@ -91,36 +109,32 @@ void accessGrantedSound() {
 }
 
 
+void accessDeniedSound() {
+  tone(BUZZER, NOTE_E7);
+  delay(50);
+  noTone(BUZZER);
+  delay(50);
+  tone(BUZZER, NOTE_E7);
+  delay(100);
+  noTone(BUZZER);
+  tone(BUZZER, NOTE_E7);
+  delay(50);
+  noTone(BUZZER);
+}
+
 void execution() {
-  lcd.clear();
-  lcd.print ("INITIATING SELF");
-  lcd.setCursor(0, 1);
-  lcd.print("DEFENCE PROTOCOL!");
-
+  printCenteredText("INITIATING SELF", "DEFENCE PROTOCOL!");
   delay(3000);
-  lcd.clear();
-  lcd.setCursor(7, 0);
-  lcd.print("3");
-
+  printCenteredText("3");
   delay(1000);
-  lcd.clear();
-  lcd.setCursor(7, 0);
-  lcd.print("2");
-
+  printCenteredText("2");
   delay(1000);
-  lcd.clear();
-  lcd.setCursor(7, 0);
-  lcd.print("1");
-
+  printCenteredText("1");
   delay(1000);
-  lcd.clear();
-  lcd.setCursor(5, 0);
-  lcd.print("DIE!!!");
+  printCenteredText("DIE!!!");
   digitalWrite(TASER_PIN, HIGH);
-
   delay (1500);
   digitalWrite(TASER_PIN, LOW);
-
   delay (1500);
   lcd.clear();
 }
@@ -128,8 +142,8 @@ void execution() {
 AuthorizedUser* currentUser;
 
 void verifyCode(AuthorizedUser* user) {
-  Serial.println("Starting code verification");
   inAuthenticationProcess = true;
+  Serial.println("Starting code verification");
   lcd.clear();
   lcd.print(verificationMessage);
   currentUser = user;
@@ -137,28 +151,66 @@ void verifyCode(AuthorizedUser* user) {
 }
 
 
+void playAccessGrantedJingle() {
+  tone(BUZZER, NOTE_E6);
+  delay(100);
+  noTone(BUZZER);
+  delay(100);
+  
+  tone(BUZZER, NOTE_G6);
+  delay(100);
+  noTone(BUZZER);
+  delay(100);
+  
+  tone(BUZZER, NOTE_A6);
+  delay(100);
+  noTone(BUZZER);
+  delay(100);
+  
+  tone(BUZZER, NOTE_B6);
+  delay(100);
+  noTone(BUZZER);
+  delay(100);
+  
+  tone(BUZZER, NOTE_E7);
+  delay(200);
+  noTone(BUZZER);
+}
+
+void playAccessDeniedJingle() {
+  tone(BUZZER, NOTE_E7);
+  delay(100);
+  noTone(BUZZER);
+  delay(100);
+
+  tone(BUZZER, NOTE_D7);
+  delay(100);
+  noTone(BUZZER);
+  delay(100);
+
+  tone(BUZZER, NOTE_C7);
+  delay(300);
+  noTone(BUZZER);
+}
+
 void verificationLoop() {
   verificationStart:
   pressedKey = keyfield.getKey();
   if (!pressedKey) { return; }
-
+  tone(BUZZER, NOTE_E7);
+  delay(50);
+  noTone(BUZZER);
   if (pressedKey == '*') {
-   Serial.println("Aborted Code verification.");
-   lcd.clear();
-   lcd.setCursor(4, 0);
-   lcd.print("Aborted");
-   lcd.setCursor(2, 1);
-   lcd.print("Verification");
-   inAuthenticationProcess = false;
-   return;
+    inAuthenticationProcess = false;
+    Serial.println("Aborted Code verification.");
+    printCenteredText("Aborted", "Verification");
+    delay(1000);
+    lcd.clear();
+    return;
   }
 
   if (pressedKey == '#') {
-    lcd.clear();
-    lcd.setCursor(5, 0);
-    lcd.print("Input");
-    lcd.setCursor(4, 1);
-    lcd.print("Resetted");
+    printCenteredText("Input", "Resetted");
     delay(2000);
     lcd.clear();
     z1=0; z2=1; z3=1; z4=1;
@@ -199,27 +251,24 @@ void verificationLoop() {
       //servoblau.write(0);
       digitalWrite(RED_LIGHT, LOW);
       digitalWrite(GREEN_LIGHT, HIGH);
-      lcd.clear();
-      lcd.setCursor(1, 0);
-      lcd.print("Access granted");
+      printCenteredText("Access granted");
       delay(300);
       String message = "Welcome " + currentUser->username;
-      int cursorPos = (16 - message.length()) / 2; 
-      lcd.setCursor(cursorPos, 1);
-      lcd.print(message);
+      printCenteredText("Access granted", message);
+      playAccessGrantedJingle();
       delay(2000);
       lcd.clear();
       currentUser = NULL;
       inAuthenticationProcess = false;
+      z1=0; z2=1; z3=1; z4=1;
+      verificationMessage = "Code: ";
     } else {
       Serial.println ("Code falsch");
       digitalWrite(RED_LIGHT, HIGH);
       digitalWrite(GREEN_LIGHT, LOW);
-      lcd.clear();
-      lcd.setCursor(1, 0);
-      lcd.print("Access denied!");
+      printCenteredText("Access denied!");
+      playAccessDeniedJingle();
       delay(2000);
-      lcd.clear();
       z1=0; z2=1; z3=1; z4=1;
       verificationMessage = "Code: ";
       lcd.clear();
@@ -230,17 +279,17 @@ void verificationLoop() {
 }
 
 
-void loop(){
+void loop() {
 
   if (inAuthenticationProcess == true) {
     verificationLoop();
     return;
   }
-  
+
   lcd.setCursor(5, 0);
   lcd.print("Welcome");
-  lcd.setCursor(2, 1);
-  lcd.print("Insert chipcard");
+  lcd.setCursor(0, 1);
+  lcd.print("Insert Chipcard");
 
   // Klingel
   if (digitalRead(KLINGEL_PIN) == HIGH) {
@@ -272,12 +321,10 @@ void loop(){
     Serial.println("Zu dieser ID wurde kein Systemeintrag gefunden.");
     Serial.print("Verbleibende Versuche: ");
     Serial.println(remainingAttempts);
-    digitalWrite (BUZZER, HIGH);
-    digitalWrite (RED_LIGHT, HIGH);
-    lcd.clear();
-    lcd.setCursor(1, 0);
-    lcd.print("Access denied!");
-    delay (2000);
+    digitalWrite(RED_LIGHT, HIGH);
+    printCenteredText("Access denied!");
+    accessDeniedSound();
+    delay(2000);
     lcd.clear();
     lcd.setCursor(3, 0);
     lcd.print("Remaining");
@@ -296,13 +343,10 @@ void loop(){
   // Access granted
   digitalWrite (GREEN_LIGHT, HIGH); 
   delay(100);
-  lcd.clear();
-  lcd.setCursor(4, 0);
-  lcd.print("Chipcard");
-  lcd.setCursor(4, 1);
-  lcd.print("Verified");
+  printCenteredText("Chipcard", "Verified");
   accessGrantedSound();
   delay(2000);
   digitalWrite (GREEN_LIGHT, LOW);
+  lcd.clear();
   verifyCode(user);
 }
